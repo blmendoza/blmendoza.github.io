0a37bd089577f1a3aa2ac8ca112a18156a9f3b65
Briana Mendoza <bripower12@gmail.com>
2020-02-04 12:56:23 -0600

    added tracker

--- calendarIdea.js
+++ calendarIdea.js
@@ -5,15 +5,23 @@ let canvasY;
 let rectHeight = 34;
 let rectWidth = 300;
 let barXPos = 202;
-let barYPos = 252;
+let barYPos = 253; //will add +50 for hunger/money bars
+let tiredWidth = 10;
+let hungerWidth = 10;
+let moneyWidth;
 let counter = 0;
 
-//moving line
-let rectYPos = 0;
+//moving tracker
+let days = ['Monday','Tuesday','Wednesday','Thursday','Friday'];
+let currentDay = days[0];
+let trackerXPos;
+let trackerYPos = 100;
 
 //calender build
 let lineVerX = 550;
-let lineHorY = 120;
+let lineHorY = 100;
+let posXList = []; //saving positions for 'day' header placements, vertical line build
+let posYList = []; //saving positiosn for 'time' header placements, horizontal line build
 
 
 //let calHead1;
@@ -62,27 +70,24 @@ function setup(){
   strokeWeight(3);
   line(lineVerX,0,lineVerX,height); //separator line
 
-  let posXList = []; //saving positions for 'day' header placements
   strokeWeight(1.5);
   stroke(128,128,128);
   lineVerX+=100;
   for(let i=0; i<6; i++){
     posXList.push(lineVerX);
-    line(lineVerX,lineHorY,lineVerX,(lineHorY+(30*19)));
+    line(lineVerX,lineHorY,lineVerX,(lineHorY+(25*23)));
     lineVerX+=150;
   }
 
-  let posYList = []; //saving positiosn for 'time' header placements
   strokeWeight(1);
   stroke(192,192,192);
-  for(let k=0; k<20; k++){
+  for(let k=0; k<24; k++){
     line(posXList[0],lineHorY,posXList[5],lineHorY);
     posYList.push(lineHorY);
-    lineHorY+=30;
+    lineHorY+=25;
   }
 
   //add day headings
-  let days = ['Monday','Tuesday','Wednesday','Thursday','Friday'];
   textSize(25);
   for(let j=0;j<5;j++){
     text(days[j],posXList[j]+10,(posYList[0]-20));
@@ -90,8 +95,8 @@ function setup(){
   console.log("finished drawing");
 
   //add time headings
-  let times = ['5AM','6AM','7AM','8AM','9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM','6PM','7PM','8PM','9PM','10PM','11PM','12AM'];
-  textSize(15);
+  let times = ['12AM','1AM','2AM','3AM','4AM','5AM','6AM','7AM','8AM','9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM','6PM','7PM','8PM','9PM','10PM','11PM','12AM'];
+  textSize(12);
   for(let l=0;l<times.length;l++){
     text(times[l],(posXList[0]-50),(posYList[l]+5));
   }
@@ -100,41 +105,82 @@ function setup(){
 function draw(){
 
   if(gameStat){
-    if(counter == 10){
-      console.log("Reached the max");
+    if((frameCount%60 == 0 && tiredWidth < 300) || (frameCount%60 ==0 && hungerWidth < 300)){ //check that either bar isn't maxed
+      //this "erases" the previous bar made for tiredness
+      stroke(255);
+      fill(255);
+      rect(barXPos,barYPos,tiredWidth,34);
+
+      //reset to a visible stroke/fill and "move" bar forward for tiredness
+      noStroke();
+      fill(255,0,0);
+      tiredWidth+=10;
+      rect(barXPos,barYPos,tiredWidth,34);
+
+      //this "erases" the previous bar made for hunger
+      stroke(255);
+      fill(255);
+      rect(barXPos,barYPos+50,hungerWidth,34);
+
+      //reset to a visible stroke/fill and "move" bar forward for hunger
+      noStroke();
+      fill(255,0,0);
+      hungerWidth+=10;
+      rect(barXPos,barYPos+50,hungerWidth,34);
+
+      //draw tracker
+      let buffer = 2;
+      if(currentDay == 'Monday'){
+        trackerXPos = posXList[0]+buffer;
+      }
+      else if(currentDay == 'Tuesday') {
+        trackerXPos = posXList[1]+buffer;
+      }
+      else if(currentDay == 'Wednesday') {
+        trackerXPos = posXList[2]+buffer;
+      }
+      else if(currentDay == 'Thursday'){
+        trackerXPos = posXList[3]+buffer;
+      }
+      else if(currentDay == 'Friday'){
+        trackerXPos = posXList[4]+buffer;
+      }
+      stroke(255,0,0);
+      fill(255,0,0);
+      let tracker = rect(trackerXPos,trackerYPos+1,146,2); //line
+
+      //past tracker erasing
+      stroke(255);
+      fill(255);
+      rect(trackerXPos,trackerYPos,146,3);
+
+      //tracker advancing
+      stroke(255,0,0);
+      fill(255,0,0);
+      trackerYPos+=10;
+      rect(trackerXPos,trackerYPos,146,2);
     }
     else{
-      if(frameCount%60 == 0 && rectWidth < 300){
-
-        //this "erases" the previous lines/rectangles made
-        stroke(255);
-        fill(255);
-        rect(10,10,rectWidth,20); //bar
-        rect(300,rectYPos,100,2); //line
-
-        counter++;
-
-        //reset to a visible stroke/fill and "move" items
-        noStroke();
-        fill(255,0,0);
-        rectWidth+=10;
-        rect(10,10,rectWidth,20);
-        rectYPos+=10;
-        rect(300,rectYPos,100,2);
-      };
+      if(tiredWidth >= 300){
+        textSize(50);
+        text('You passed out from exhaustion, game over.',0,500,500,height);
+        gameStat = false;
+      }
+      else if(hungerWidth >= 300){
+        textSize(50);
+        text('You passed out from hunger, game over.',0,500,500,height);
+        gameStat = false;
+      }
     }
-  }
-  else{
 
   }
-
 }
 
-/*
-function changeCalHead(calHead){
-  image(calHead, canvasX, canvasY, 100, 100);
+function keyPressed(){
+  if(keyCode==32){
+    gameStat = true;
+  }
 }
-*/
 
 function mousePressed(){
   //"erases" old bar
