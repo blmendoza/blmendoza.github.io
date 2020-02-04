6b689fbf68a38e2f076588d6f30cee9e6882a766
Briana Mendoza <bripower12@gmail.com>
2020-02-04 11:08:14 -0600

    adjusted and added bar outlines

--- calendarIdea.js
+++ calendarIdea.js
@@ -13,7 +13,7 @@ let rectYPos = 0;
 
 //calender build
 let lineVerX = 550;
-let lineHorY = 159;
+let lineHorY = 120;
 
 
 //let calHead1;
@@ -22,6 +22,7 @@ let lineHorY = 159;
 //let currentCalHead;
 let testImg;
 let gameStat=false;
+
 
 function preload(){ //loads image before any js runs
   //calHead1 = loadImage('images/morning.png');
@@ -46,7 +47,14 @@ function setup(){
   //counter setup
   stroke(255,0,0);
   noFill();
-  rect(200,250,rectWidth+2,40);
+  textSize(40);
+  let textPos = 280;
+  text('Tiredness',0,textPos);
+  rect(200,250,rectWidth+2,40); //for sleep counter
+  text('Hunger',0,textPos+50);
+  rect(200,300,rectWidth+2,40); //for hunger counter
+  text('Money',0,textPos+100);
+  rect(200,350,rectWidth+2,40); //for money counter
 
 
   //draws calendar outline
@@ -58,32 +66,35 @@ function setup(){
   strokeWeight(1.5);
   stroke(128,128,128);
   lineVerX+=100;
-  for(let i=0; i<8; i++){
-    line(lineVerX,159,lineVerX,height);
-    lineVerX+=200;
+  for(let i=0; i<6; i++){
     posXList.push(lineVerX);
+    line(lineVerX,lineHorY,lineVerX,(lineHorY+(30*19)));
+    lineVerX+=150;
   }
 
   let posYList = []; //saving positiosn for 'time' header placements
   strokeWeight(1);
   stroke(192,192,192);
   for(let k=0; k<20; k++){
-    line(posXList[k],lineHorY,width,lineHorY);
+    line(posXList[0],lineHorY,posXList[5],lineHorY);
     posYList.push(lineHorY);
     lineHorY+=30;
   }
 
   //add day headings
-  let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+  let days = ['Monday','Tuesday','Wednesday','Thursday','Friday'];
   textSize(25);
-  for(let j=0;j<7;j++){
-    text(days[j],posXList[j]+10,100);
+  for(let j=0;j<5;j++){
+    text(days[j],posXList[j]+10,(posYList[0]-20));
   }
   console.log("finished drawing");
 
   //add time headings
   let times = ['5AM','6AM','7AM','8AM','9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM','6PM','7PM','8PM','9PM','10PM','11PM','12AM'];
-
+  textSize(15);
+  for(let l=0;l<times.length;l++){
+    text(times[l],(posXList[0]-50),(posYList[l]+5));
+  }
 }
 
 function draw(){
